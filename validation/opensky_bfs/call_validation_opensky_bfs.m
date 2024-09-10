170d638389a7f2ae2cd8ebb5d7748a27d791bc81
andrepiz <andpiz1997@gmail.com>
2024-09-10 11:57:04 +0200

    [NEW] Added validation with open-sky image and retail camera; re-structured code and folders

---
+++ validation/opensky_bfs/call_validation_opensky_bfs.m
@@ -0,0 +1,19 @@
+abram_install()
+
+%% Choose camera
+flag_camera = 1; % Camera 1: 50 mm, Camera 2: 25 mm
+
+%% Inputs
+inputs_validation_opensky_bfs();     
+
+%% Run Model
+run_model();
+
+%% Post-processing
+if flag_camera == 1
+    image_filepath_real = fullfile('data','50mm.png');
+elseif flag_camera == 2
+    image_filepath_real = fullfile('data','25mm.png');
+end
+postpro_validation_opensky_bfs();
+postpro_3d();
