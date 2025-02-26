26cb8ea0a89263c9c3b0757fb57e0ad080f1b272
andrepiz <andpiz1997@gmail.com>
2025-02-26 16:53:05 +0100

    [NEW] Added render engine calibration script

--- test/application/render_engine_calibration/main_render_engine_calibration.m
+++ test/application/render_engine_calibration/main_render_engine_calibration.m
@@ -1,5 +1,5 @@
 %% RADIOMETRIC CALIBRATION OF RENDER ENGINES
-% Render a 1m-radius, 0.1 albedo, Lambertian sphere placed at 1 AU of distance from the 
+% Render a 1m-radius, 0.01 albedo, Lambertian sphere placed at 1 AU of distance from the 
 % Sun and different phase angles and at a common exposure time such to
 % avoid saturation.
 % Then perform the calibration comparing the electron count of
@@ -13,16 +13,13 @@ abram_install();
 
 %% GROUND TRUTH
 % USE YML
-%filename_yml = 'amie_sphere01_128m_30ms_10bit.yml';
-%filename_yml = 'aastr_sphere001_8m_02ms_8bit.yml';
-filename_yml = 'cicd_sphere1_128m_02ms_16bit.yml';
+filename_yml = 'sphere001_8m_02ms_8bit.yml';
 
 inputs_yml();
 
 run_model();
 
 %% POST-PRO
-%image_filepath_render_engine = 'data\aastr_sphere001_8m_02ms_8bit\img\000001.png';
-image_filepath_render_engine = 'data\cicd_sphere1_64m_3ms_16bit\img\000000.png';
+image_filepath_render_engine = 'data\sphere001_8m_02ms_8bit\img\000001.png';
 
 postpro_render_engine_calibration();
