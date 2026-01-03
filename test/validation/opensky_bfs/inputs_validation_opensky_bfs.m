95575c47710c56bf6ab202a26feed7035275334d
andrepiz <andpiz1997@gmail.com>
2026-01-02 18:22:26 -0800

    [FIX] Small fixed and files reorg

--- test/validation/opensky_bfs/inputs_validation_opensky_bfs.m
+++ test/validation/opensky_bfs/inputs_validation_opensky_bfs.m
@@ -1,8 +1,35 @@
+%% BODY
+rend.body.maps.albedo.filename = 'moon\lroc_cgi\lroc_color_16bit_srgb_8k.tif';
+rend.body.maps.albedo.depth = 16;
+if flag_hapke
+    rend.body.albedo = 0.51; %0.18;
+    rend.body.radiometry.model = 'hapke'; 
+    rend.body.radiometry.parameters = [0.25, 0.3, 0, 1, 2.2, 0.07, 0.4129, 0];     % Found from Sato et al. Bandwidth: 450-1150 nm
+    rend.body.maps.albedo.domain = [0.12 0.75];% + 0.3;
+else
+    rend.body.albedo = 0.20; %0.18
+    rend.body.radiometry.model = 'oren'; 
+    rend.body.radiometry.roughness = 0.3;     
+    rend.body.maps.albedo.mean = rend.body.albedo;    
+end          
+if flag_displacement
+    rend.body.maps.displacement.filename = 'moon\ldem_4.tif';
+    rend.body.maps.displacement.depth = 1;
+    rend.body.maps.displacement.scale = 1000;
+end
+if flag_normal
+    rend.body.maps.normal.filename = 'moon\Moon_LRO_LOLA_NBM_Global_4ppd_pizzetti2025.tif';
+    rend.body.maps.normal.depth = 32;
+    rend.body.maps.normal.frame = 'body';
+end
+
+%% SCENE
+
 if flag_scenario == 1       % 50 mm
-    ampl_DB = 15; % metadata
-    fNum = 4; % metadata
-    tExp = 114e-6; % metadata
-    f = 50e-3;  % data
+    rend.camera.amplification = 15; % metadata
+    rend.camera.fNum = 4; % metadata
+    rend.camera.tExp = 114e-6; % metadata
+    rend.camera.f = 50e-3;  % data
     image_filepath_real = fullfile('2023-12-22_21_29_53_moon50','imgFLIR_20231222212953_115.png');
     time_UTC = "2023/12/22 21:29:53";
     if flag_use_spice
@@ -13,21 +40,21 @@ if flag_scenario == 1       % 50 mm
                         2.629864326084645]; % X+: increase vertical Y+: decrease horizontal Z+: rotate hourly
     else
         AU = 149597870707; % [m]
-        d_body2star = 1*AU;
+        rend.scene.d_body2light = 1*AU;
         % taken from https://www.mooncalc.org/
         percAreaIlluminated = 0.819; % 
-        phase_angle = acos(2*percAreaIlluminated - 1);
-        d_body2cam = 378396e3;
+        rend.scene.phase_angle = acos(2*percAreaIlluminated - 1);
+        rend.scene.d_body2cam = 378396e3;
         % to fine-tune:
-        rpy_CAMI2CAM = [deg2rad(0.44);deg2rad(-0.48);deg2rad(145)];                     % [rad] Camera euler angles off-pointing (rpy). Note that yaw is optical axis
-        rpy_CSF2IAU = [deg2rad(-179.7006);deg2rad(2.0779);deg2rad(54.5703)];                     % [rad] 
+        rend.scene.rpy_CAMI2CAM = [deg2rad(0.44);deg2rad(-0.48);deg2rad(145)];                     % [rad] Camera euler angles off-pointing (rpy). Note that yaw is optical axis
+        rend.scene.rpy_CSF2IAU = [deg2rad(-179.7006);deg2rad(2.0779);deg2rad(54.5703)];                     % [rad] 
      end
 
 elseif flag_scenario == 2   % 25 mm
-    ampl_DB = 15; % metadata
-    fNum = 4; % metadata
-    tExp = 80e-6; % metadata
-    f = 25e-3;  % data
+    rend.camera.amplification = 15; % metadata
+    rend.camera.fNum = 4; % metadata
+    rend.camera.tExp = 80e-6; % metadata
+    rend.camera.f = 25e-3;  % data
     image_filepath_real = fullfile('2023-12-22_19_21_24_moon','imgFLIR_20231222192125_025.png');
     time_UTC = "2023/12/22 19:21:25";
     if flag_use_spice
@@ -42,148 +69,47 @@ elseif flag_scenario == 2   % 25 mm
         % to fine-tune
         AU = 149597870707; % [m]
         percAreaIlluminated = 0.819; % https://www.mooncalc.org/
-        phase_angle = acos(2*percAreaIlluminated - 1);
-        d_body2star = 1*AU;
-        d_body2cam = 378396e3;
-        rpy_CAMI2CAM = [deg2rad(-0.97);deg2rad(0.42);deg2rad(184)];                     % [rad] Camera euler angles off-pointing (rpy). Note that yaw is optical axis
-        rpy_CSF2IAU = [deg2rad(-179.7006);deg2rad(2.0779);deg2rad(54.5703)];                     % [rad] 
+        rend.scene.phase_angle = acos(2*percAreaIlluminated - 1);
+        rend.scene.d_body2light = 1*AU;
+        rend.scene.d_body2cam = 378396e3;
+        rend.scene.rpy_CAMI2CAM = [deg2rad(-0.97);deg2rad(0.42);deg2rad(184)];                     % [rad] Camera euler angles off-pointing (rpy). Note that yaw is optical axis
+        rend.scene.rpy_CSF2IAU = [deg2rad(-179.7006);deg2rad(2.0779);deg2rad(54.5703)];                     % [rad] 
      end
 end
 
-%% STAR 
-% Select star
-Tstar = 5782;     % [K]
-Rstar = 695000e3; % [m]
-star_type = 'bb';
-
-%% BODY
-Rbody = 1.7374e6; % [m]
-albedo_type = 'bond';
-if flag_hapke
-    albedo = 0.20; %0.18;
-    radiometry_model = 'hapke'; 
-    radiometry_parameters = [0.25, 0.25, 0, 1, 1.8, 0.07, 0.4084, 1];     % Found from Sato et al. Bandwidth: 450-1150 nm
-else
-    albedo = 0.20; %0.18
-    radiometry_model = 'oren'; 
-    radiometry_roughness = 0.3;                                           % roughness in oren model (>> more rough)
-end    
-albedo_filename = 'moon\lroc_cgi\lroc_color_poles_2k.tif';
-albedo_depth = 8;
-albedo_mean = albedo;          
-if flag_displacement
-    displacement_filename = 'moon\ldem_4.tif';
-    displacement_depth = 1;
-    displacement_scale = 1000;
-end
-if flag_normal
-    normal_filename = 'moon\Moon_LRO_LOLA_NBM_Global_4ppd_pizzetti2025.tif';
-    normal_depth = 32;
-    normal_frame = 'body';
-end
 
 %% CAMERA
 
-% BFS-PGE-31S4M-C Sony IMX265
-dpupil = f/fNum;
-muPixel = 3.45e-6; % [m] pixel size  
-res_px = [2048 1536]; % [px] Resolution in pixel 
-fov = 2*atan((res_px*muPixel/2)/f); % focal length
-fov_shape = 'frustum';
-
-QE_lambda_min = [2.72e-07,3.22e-07,3.72e-07,4.22e-07,4.72e-07,5.22e-07,5.72e-07,6.22e-07,6.72e-07,7.22e-07,7.72e-07,8.22e-07,8.72e-07,9.22e-07,9.72e-07,1.022e-06,1.072e-06];
-QE_lambda_max = [3.22e-07,3.72e-07,4.22e-07,4.72e-07,5.22e-07,5.72e-07,6.22e-07,6.72e-07,7.22e-07,7.72e-07,8.22e-07,8.72e-07,9.22e-07,9.72e-07,1.022e-06,1.072e-06,1.122e-06];
-QE_values = [0.046335,0.21472,0.46362,0.63806,0.66967,0.68317,0.62862,0.56362,0.46565,0.35428,0.26281,0.16923,0.10733,0.057816,0.028469,0.011971,0];
-QE_sampling = 'piecewise';
-
-nbw = length(QE_lambda_min);
-T_lambda_min = QE_lambda_min;
-T_lambda_max = QE_lambda_max;
 if flag_scenario == 1
-    T_values = 0.6*ones(1, nbw);    
+    rend.camera.T.values = 0.6*rend.camera.T.values;    
 else
-    T_values = 0.95*ones(1, nbw); % transmittance of 25 mm is 1.5 times the 50 mm. Estimated through visual check of pattern using same fNum and exposure time.
+    rend.camera.T.values = 0.95*rend.camera.T.values; % transmittance of 25 mm is 1.5 times the 50 mm. Estimated through visual check of pattern using same fNum and exposure time.
 end
-T_sampling = 'piecewise';
 
 % BFS-PGE-31S4M-C Sony IMX265
 gain_electron2adu = 5.88;       % 16 bit DN over electrons 
 adc_depth = 12;
-fwc = 10760;  
-G_AD = gain_electron2adu/(2^16-1)*(2^adc_depth-1);
-amplification = ampl_DB;
 if flag_account_for_atmosphere
     atm_red_factor = 0.85; % assumed a reduction factor caused by the atmosphere on the gain. From "On the radiometric calibration of optical Hardware-In-the-Loop stimulators", Ornati et al.
 else
     atm_red_factor = 1;
 end
-G_AD = atm_red_factor*G_AD;
-offset = 0;
-% snr_DB = 40.32;
-% snr_bits = 6.7;
-% dnr_DB = 71.69;
-% dnr_bits = 11.91;
-%G_AD = (2^dnr_bits)/fwc*ampl_lin;
-
-noise.shot.flag = false;
-noise.shot.sigma = 0;
-noise.shot.seed = [];
-noise.prnu.flag = false;
-noise.prnu.sigma = 0;
-noise.prnu.seed = [];
-noise.dark.flag = false;    
-noise.dark.sigma = 0;
-noise.dark.mean = 0;
-noise.dark.seed = [];
-noise.readout.flag = false;
-noise.readout.sigma = 0;
-noise.readout.seed = [];
-
-distortion.radial = [];
-distortion.decentering = [];
+rend.camera.G_AD  = atm_red_factor*gain_electron2adu/(2^16-1)*(2^adc_depth-1);
+rend.camera.offset = 0;
 
 %% SCENARIO
 if flag_use_spice
     q_ENU2CAM = euler_to_quat(rpy_ENU2CAM);    
-    [d_body2star, d_body2cam, phase_angle, q_CAMI2CAM, q_CSF2IAU] = ...
+    [rend.scene.d_body2light, rend.scene.d_body2cam, rend.scene.phase_angle, q_CAMI2CAM, q_CSF2IAU] = ...
                 inputs_ecef2matlab(time_UTC, posCam_altlonlat, q_ENU2CAM, filename_metakernel, 'MOON');
-    rpy_CSF2IAU = quat_to_euler(q_CSF2IAU);
-    rpy_CAMI2CAM = quat_to_euler(q_CAMI2CAM);
+    rend.scene.rpy_CSF2IAU = quat_to_euler(q_CSF2IAU);
+    rend.scene.rpy_CAMI2CAM = quat_to_euler(q_CAMI2CAM);
 end
 
-%% PARAMETERS
-% Select parameters
-general_environment = 'matlab';
-general_workers = 4;
-general_parallelization = true;
-discretization_method = 'adaptive';
-discretization_accuracy = 10;
-discretization_np = 5e5;            % number of total pixel sectors on sphere
-sampling_method = 'projecteduniform'; % 'projective' sampling of longitude and latitude points that are approximately spread uniformly on the projected sphere
-sampling_ignore_unobservable = true;             % ignore sectors that fall outside the tangency circle
-sampling_ignore_occluded = true;             % ignore sectors that fall outside the tangency circle
-sampling_occlusion_rays = 10;
-sampling_occlusion_angle = 'auto';
-integration_method = 'trapz';           
-integration_np = 10;                % number of evaluation points for trapz
-integration_correct_incidence = true;    % Correct incidence angle with true incidence angle with low distance-to-radius ratios
-integration_correct_reflection = true;    % Correct incidence angle with true incidence angle with low distance-to-radius ratios
-gridding_method = 'weightedsum';        % 'weightedsum' weighs each intensity value spreading it across the neighbouring pixels
-gridding_algorithm = 'gaussian';
-gridding_scheme = 'linear'; 
-gridding_window = 1;
-gridding_shift = 1;
-gridding_filter = 'gaussian';
-reconstruction_granularity = 'auto';                        % Down-sampling of pixels
-reconstruction_antialiasing = true;                        
-reconstruction_filter = 'bilinear';                        
-processing_distortion = false;
-processing_diffraction = false;
-processing_noise = false;
-processing_blooming = false;
-saving_filename = 'validation_opensky_bfs';
-saving_format = 'png';
-saving_depth = adc_depth;
-
-%% PRE-PROCESSING
-prepro()
+%% SETTING
+rend.setting.saving.depth = adc_depth;
+rend.setting.discretization.method = 'fixed';
+rend.setting.discretization.np = 1e6;
+%rend.setting.integration.method = 'trapz';
+rend.setting.gridding.window = 2;
+rend.setting.gridding.sigma = 1;
