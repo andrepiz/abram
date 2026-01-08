a497ae95b1100e3c09e6903ace381b7d48dce8e2
andrepiz <andpiz1997@gmail.com>
2026-01-07 20:36:56 -0800

    [NEW] Added dawn validation script and moon landing tutorial

--- test/tutorial/moon_landing/call_moon_landing.m
+++ test/tutorial/moon_landing/call_moon_landing.m
@@ -1,31 +1,40 @@
 abram_install()
 
+flag_show_footprint = false;
+
+%% SET OBJECT
 rend = abram.render('moon_landing.yml');
 
-%% LANDING SCENARIO [50 km - 10 m]
+%% LANDING SCENARIO [1500 km - 50 m]
 h0 = rend.altitude;
 d0 = rend.scene.d_body2cam;
 R0 = rend.bodyRadiusAtNadir;
 
+rend.setting.saving.filename = 'results\moon_landing';
+rend.setting.saving.filename = [];
 rend.setting.discretization.accuracy = 'medium';
-%rend.setting.discretization.accuracy = 0.1; % debug
+rend.setting.discretization.accuracy = 1; % debug to test a new trajectory
 
 nPlot = 30; 
-d_body2cam_vec = interp1(linspace(0, 1, 6), R0 + 1e3*[60 30 10 1 0.1 0.01], linspace(0, 1, nPlot));
-rpy_CAMI2CAM_vec = interp1(linspace(0, 1, 6), [0.7 1.1 1.3 1.5 1.55 1.6; 0.2 0.4 0.6 0.8 0.8 0.8; 0.1 0.3 0.4 0.6 0.6 0.6]', linspace(0, 1, nPlot))';
+d_body2cam_waypts = R0 + 1e3*[1500 800 400 200 100 60 30 10 1 0.1 0.015];
+rpy_CAMI2CAM_waypts = [0.1 0.2   0.4  0.65  0.95 1.1  1.2 1.35 1.48 1.55 1.6; ...
+                       0   0.05  0.1  0.15  0.2  0.25 0.3 0.35 0.4 0.45 0.5; ...
+                       0   0.05  0.1  0.15  0.2  0.25 0.3 0.35 0.4 0.45 0.5];
+d_body2cam_vec = interp1(linspace(0, 1, length(d_body2cam_waypts)), d_body2cam_waypts, linspace(0, 1, nPlot));
+rpy_CAMI2CAM_vec = interp1(linspace(0, 1, length(d_body2cam_waypts)), rpy_CAMI2CAM_waypts', linspace(0, 1, nPlot))';
 
-%rend.camera.res_px = [1366 768]; % cinematic
+% rend.camera.res_px = [1366 768]; % cinematic
 
 idxPlot = 1:nPlot;
-%idxPlot = 18:nPlot;
 
 lonMin = nan;
 lonMax = nan;
 latMin = nan;
 latMax = nan;
 
-% rend.scene.rpy_CSF2IAU = [0; 0; 0.9-1.3265+0.1047];
-rend.update_sectors = true;
+fh1 = figure();
+axIm = gca();
+im = imshow(digital2digital(rend.img, rend.setting.saving.depth, 8),'Parent',axIm);
 
 for ix = idxPlot
 
@@ -36,14 +45,19 @@ fprintf(['\nALTITUDE: ', num2str(rend.altitude), ' METERS'])
 
 rend = rend.rendering();
 
+im = imshow(digital2digital(rend.img, rend.setting.saving.depth, 8),'Parent',axIm);
+drawnow
+
+if flag_show_footprint
 [lonMin_temp, lonMax_temp, latMin_temp, latMax_temp] = rend.footprint();
 lonMin = min(lonMin_temp, lonMin);
 lonMax = max(lonMax_temp, lonMax);
 latMin = min(latMin_temp, latMin);
 latMax = max(latMax_temp, latMax);
 
-fprintf(['\nFOOTPRINT: ', num2str(rad2deg(lonMin_temp)),'/',num2str(rad2deg(lonMax_temp)),' LON, ', num2str(rad2deg(latMin_temp)),'/',num2str(rad2deg(latMax_temp)),' LAT'])
-fprintf(['\nTRAJECTORY FOOTPRINT: ', num2str(rad2deg(lonMin)),'/',num2str(rad2deg(lonMax)),' LON, ', num2str(rad2deg(latMin)),'/',num2str(rad2deg(latMax)),' LAT'])
+fprintf(['\nCURRENT IMAGE FOOTPRINT: ', num2str(rad2deg(lonMin_temp)),'/',num2str(rad2deg(lonMax_temp)),' LON, ', num2str(rad2deg(latMin_temp)),'/',num2str(rad2deg(latMax_temp)),' LAT'])
+fprintf(['\nFULL TRAJECTORY FOOTPRINT: ', num2str(rad2deg(lonMin)),'/',num2str(rad2deg(lonMax)),' LON, ', num2str(rad2deg(latMin)),'/',num2str(rad2deg(latMax)),' LAT'])
+end
 
 end
 
