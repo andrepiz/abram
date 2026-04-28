dd5997f702abdd9e592deb167697ce6f0c64859c
andrepiz <andpiz1997@gmail.com>
2026-04-28 15:19:15 +0200

    [v1.8] Modified abram_install version number

--- abram_install.m
+++ abram_install.m
@@ -70,4 +70,4 @@ if isfolder(debug_path)
 end
 
 % DISPLAY
-fprintf(['*** ABRAM 1.7 installed in ', op_sys, '. Have fun! ***\n'])
+fprintf(['*** ABRAM 1.8 installed in ', op_sys, '. Have fun! ***\n'])
